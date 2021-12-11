ca45ba7da166dc6ad4ad94c492e3ff4bcac346c7
thangpdth2009031@outlook.com.vn <thangpdth2009031@outlook.com.vn>
2021-12-11 11:55:52 +0700

    add file

---
+++ AssigmentWADPDT/Models/SinhVien.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AssigmentWADPDT.Models
+{
+    public class SinhVien
+    {
+        [Key]
+        public string MaSinhVien { get; set; }
+        public string HinhThuc { get; set; }
+        public int SoLuong { get; set; }
+        public DateTime NgayNopPhat
+        {
+            get { return DateTime.Now; }
+        }
+    }
+}
